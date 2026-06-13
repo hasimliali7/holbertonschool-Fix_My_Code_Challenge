b3efadba72eb5eaf9d3f729c895d4b7ae80a7a16
hasimliali7 <hasimliali07@gmail.com>
2026-06-13 16:56:10 +0400

    Update 1-print_square.js

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -23,29 +23,4 @@ for (let i = 0; i < size; i++) {
     process.stdout.write('#');
   }
   process.stdout.write('\n');
-}/**
- * Print Square Module
- *
- * This script takes one argument from the command line representing
- * the size of the square and prints it using '#' characters.
- */
-
-if (process.argv.length <= 2) {
-  process.stderr.write('Missing argument\n');
-  process.stderr.write('Usage: ./1-print_square.js <size>\n');
-  process.exit(1);
-}
-
-const size = parseInt(process.argv[2], 10);
-
-if (isNaN(size)) {
-  process.stderr.write('Argument must be a number\n');
-  process.exit(1);
-}
-
-for (let i = 0; i < size; i++) {
-  for (let j = 0; j < size; j++) {
-    process.stdout.write('#');
-  }
-  process.stdout.write('\n');
 }
